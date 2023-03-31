eca74c85bfaff7d7d8ee73b7487844e4bb1f5672
chenchuang <1129989625@qq.com>
2023-03-31 10:43:30 +0800

    第一次提交

---
+++ src/main/java/com/cc/cctest/CcTestApplication.java
@@ -0,0 +1,13 @@
+package com.cc.cctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcTestApplication.class, args);
+    }
+
+}
